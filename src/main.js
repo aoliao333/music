bcbac465ecdefaa441a582609ff00bf21d71112a
圆码 <2087267149@qq.com>
2021-04-12 17:29:53 +0800

    element-ui

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import Vant from "vant";
 import "vant/lib/index.css";
 import router from "./router";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(Vant);
-
+Vue.use(ElementUI);
 new Vue({
   render: (h) => h(App),
   router,
