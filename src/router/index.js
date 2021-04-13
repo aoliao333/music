a86a58b8e9d432cbaebad1efb7bf08e1a4155a18
圆码 <2087267149@qq.com>
2021-04-13 08:20:26 +0800

    主路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
+//名字未能语义化，请看注释，分路由的子路由放在models下
+// 自己语义化命名 比如home路由的子路由，写为homechildren
+// 写完后导入此文件中 children:导入文件的名字
+
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+//例如home的子路由
+import homechild from "../router/models/homechildren";
 Vue.use(VueRouter);
+
 
 // 配置路由
 const router = new VueRouter({
@@ -11,17 +18,22 @@ const router = new VueRouter({
       path: "/",
       redirect: "/home",
     },
-    {
+
+    { //头部导航第二个 首页
       path: "/home",
-      name: "Name",
+      name: "Home",
       component: () => import("../views/home/index.vue"), // 路由懒加载
       meta: {
-        title: "我的首页",
+        title: "首页",
         showTabbar: true, //   登录前不显示底部导航
       },
+      //home 子路由引入方法
+      redirect: "/home/music",
+      children: homechild,
     },
     {
-      path: "/fenlei",
+
+      path: "/fenlei", //头部导航第一个 分类
       name: "Fenlei",
       component: () => import("../views/fenlei/index.vue"), // 路由懒加载
       meta: {
@@ -29,7 +41,8 @@ const router = new VueRouter({
       },
     },
     {
-      path: "/cart",
+
+      path: "/cart",  //头部导航第三个  音乐动态
       name: "Cart",
       component: () => import("../views/cart/index.vue"), // 路由懒加载
       meta: {
@@ -37,7 +50,5 @@ const router = new VueRouter({
       },
     },
   ],
-  linkActiveClass: "active",
 });
-
 export default router;
