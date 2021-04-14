07407db01f55c8c488cf2269ed90bfbde1a6b647
圆码 <2087267149@qq.com>
2021-04-14 08:33:49 +0800

    首页健全

--- src/api/home.js
+++ src/api/home.js
@@ -3,3 +3,14 @@ import { get } from "../utils/request";
 export const reqSwipe = (data) => get("/banner", data);
 //推荐歌单
 export const reqPersonalized = (data) => get("/personalized", data);
+//最新音乐
+export const reqNewsong = (data) => get("/personalized/newsong", data);
+// /personalized/privatecontent/list
+// 独家放送
+export const reqPrivatecontent = (data) => get("/personalized/privatecontent/list", data);
+// /personalized/mv
+//推荐MV
+export const reqMv = (data) => get("/personalized/mv", data);
+//主播电台
+export const reqDjprogram = (data) => get("/personalized/djprogram", data);
+
